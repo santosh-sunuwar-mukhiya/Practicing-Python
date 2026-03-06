82097c078d279b30f1ce9fcdee4e569bc100b308
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-06 16:49:25 +0545

    making quiz app: initial setup file

---
+++ quiz.py
@@ -0,0 +1,17 @@
+#making quiz app using 2D array.
+# Python quiz game
+
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in the human body?: ",
+             "Which planet in the solar system is the hottest?: ")
+
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+
+answers = ()
+guesses = []
