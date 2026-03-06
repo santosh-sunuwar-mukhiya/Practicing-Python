e6f1d31c948616b35cada0e5074f4c0422c1f1ab
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-06 16:25:14 +0545

    completed the shopping cart project

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,2 +1,25 @@
 #shopping cart program
 
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter food you want to buy (q/Q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("-" * 5, "YOUR CART", "-" * 5)
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total is: ${total}")
+print("-" * 20)
