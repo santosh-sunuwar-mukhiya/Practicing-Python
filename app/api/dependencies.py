0cfa432befae047d808e4499122d39e7b2f9cce2
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-29 09:42:34 +0545

    feat: add seller service dependency injection

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -3,6 +3,8 @@ from app.database.session import get_session
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from typing import Annotated
+
+from app.services.seller import SellerService
 from app.services.shipment import ShipmentService
 
 SessionDep = Annotated[Session, Depends(get_session)]
@@ -11,5 +13,12 @@ SessionDep = Annotated[Session, Depends(get_session)]
 def get_shipment_service(session:SessionDep):
     return ShipmentService(session)
 
+# Seller Service Dependency
+def get_seller_service(session: SessionDep):
+    return SellerService(session)
+
 # Shipment Service Dependency Annotation
 ShipmentServiceDep = Annotated[ShipmentService, Depends(get_shipment_service)]
+
+# Seller Service Dependency Annotation
+SellerServiceDep = Annotated[SellerService, Depends(get_seller_service)]
