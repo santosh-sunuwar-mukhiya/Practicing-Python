134ee0265f33cc052f90fbd5abf3a563f5ba6883
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-31 10:49:52 +0545

    feat: add abstraction example demonstrating abstract classes and methods

---
+++ practice/python/basics/abstraction.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def area(self):
+        return 3.14 * 5 * 5
+
+c = Circle()
+print(c.area())
