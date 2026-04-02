271f2d23546971d8850a820522b57282db8fb962
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-03 02:50:33 +0545

    feat(seller-auth): add token endpoint and seller credential verification using Argon2

--- app/api/routers/seller.py
+++ app/api/routers/seller.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import  OAuth2PasswordRequestForm
 
 from app.api.dependencies import SellerServiceDep
 from app.api.schemas.seller import SellerRead, SellerCreate
 
 router = APIRouter(prefix="/seller", tags=["seller"])
 
+# Register a Seller.
 @router.post("/signup", response_model=SellerRead)
 def register_seller(seller: SellerCreate, service: SellerServiceDep):
     return service.add(seller)
+
+# Login a Seller
+@router.post("/token")
+def login_seller(request_form: Annotated[OAuth2PasswordRequestForm, Depends()], service: SellerServiceDep):
+    token = service.token(request_form.username, request_form.password)
+    return {"access_token": token, "token_type": "bearer"}
--- app/services/seller.py
+++ app/services/seller.py
@@ -1,20 +1,42 @@
+from fastapi import HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.api.schemas.seller import SellerCreate
 from passlib.context import CryptContext
 from app.database.models import Seller
+from app.utils import generate_access_token
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 class SellerService:
-    def __init__(self, session: Session):
-        self.session  =  Session
+    def __init__(self, session: Session): # type: ignore
+        self.session  =  session
 
     def add(self, credentials: SellerCreate) -> Seller:
-        seller = Seller(**credentials.model_dump(exclude=["password"]),
+        seller = Seller(**credentials.model_dump(exclude=["password"]), # type: ignore
         password_hash=password_context.hash(credentials.password), )
 
-        self.session.add(seller)
-        self.session.commit()
-        self.session.refresh(seller)
+        self.session.add(seller) # type: ignore
+        self.session.commit()   # type: ignore
+        self.session.refresh(seller)    # type: ignore
 
         return seller
+
+    def token(self, email, password):
+        result = self.session.execute(select(Seller).where(Seller.email == email)) # type: ignore
+        seller = result.scalar()
+
+        if not seller or not password_context.verify(password, seller.password_hash,):
+            raise HTTPException(
+                status_code = status.HTTP_401_NOT_UNAUTHORIZED,
+                detail = "Email or Password is incorrect"
+            )
+
+        token = generate_access_token(
+            data ={ "user" :{
+            "name": seller.name,
+            "id": seller.id,
+        }}
+        )
+
+        return token
