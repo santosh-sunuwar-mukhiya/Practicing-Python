2223e20fa3b4e4fdc8485c3aa471489e6d886b26
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-03 17:41:54 +0545

    feat(api): add access-token and current-seller dependencies

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,4 +1,7 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+
+from app.core.security import oauth2_scheme
+from app.database.models import Seller
 from app.database.session import get_session
 from fastapi import Depends
 from sqlalchemy.orm import Session
@@ -6,8 +9,23 @@ from typing import Annotated
 
 from app.services.seller import SellerService
 from app.services.shipment import ShipmentService
+from app.utils import decode_access_token
 
 SessionDep = Annotated[Session, Depends(get_session)]
+
+def get_access_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    data = decode_access_token(token)
+
+    if data is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired access token",
+        )
+
+    return data
+
+def get_current_seller(token_data: Annotated[dict, Depends(get_access_token)], session: SessionDep):
+    return session.get(Seller, token_data["user"]["id"])
 
 # Shipment Service Dependency
 def get_shipment_service(session:SessionDep):
@@ -17,6 +35,8 @@ def get_shipment_service(session:SessionDep):
 def get_seller_service(session: SessionDep):
     return SellerService(session)
 
+# Seller Dep
+SellerDep = Annotated[ShipmentService, Depends(get_current_seller)]
 # Shipment Service Dependency Annotation
 ShipmentServiceDep = Annotated[ShipmentService, Depends(get_shipment_service)]
 
