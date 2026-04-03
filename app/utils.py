db387ed1eb8ca9c859700e327961ffea2d8ef684
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-03 17:41:39 +0545

    feat(auth): add JWT access token encode/decode helpers

---
+++ app/utils.py
@@ -0,0 +1,28 @@
+import jwt
+
+from datetime import datetime, timedelta, timezone
+
+from app.config import security_settings
+
+def generate_access_token(
+        data: dict,
+        expiry: timedelta = timedelta(days=7),
+) -> str:
+    return jwt.encode(
+        payload = {
+            **data,
+            "exp": datetime.now(timezone.utc) + expiry
+        },
+        algorithm=security_settings.JWT_ALGORITHM,
+        key=security_settings.JWT_SECRET,
+    )
+
+def decode_access_token(token: str) -> dict | None:
+    try:
+        return jwt.decode(
+            jwt=token,
+            key=security_settings.JWT_SECRET,
+            algorithms=[security_settings.JWT_ALGORITHM]
+        )
+    except jwt.PyJWTError:
+        return None
